259869668827e068b9289f4b7316f6ee1d144972
Daniel Urra <danielurra75@gmail.com>
2024-03-15 06:07:13 -0500

    Add files via upload

---
+++ capitalizationappApp.swift
@@ -0,0 +1,17 @@
+//
+//  capitalizationappApp.swift
+//  capitalizationapp
+//
+//  Created by Eusebio Taba on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct capitalizationappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
